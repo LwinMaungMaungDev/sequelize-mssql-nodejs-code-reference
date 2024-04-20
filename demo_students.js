374380d32a3de2ff3a49b11751452159694a3d7c
LwinDev <lwinmaung.maung@alliance-mm.com>
2024-04-20 18:50:14 +0630

    Query functions completed

---
+++ demo_students.js
@@ -0,0 +1,40 @@
+module.exports = [
+  {
+    name: 'Li Fei',
+    favorite_class: 'JavaScript Data Structure and Algorithms',
+    school_year: 1,
+    subscribed_to_gorgeous_cyber: true,
+  },
+  {
+    name: 'Mao Jia Su',
+    favorite_class: 'Chinese HSK 3',
+    school_year: 10,
+    subscribed_to_gorgeous_cyber: true,
+  },
+  {
+    name: 'Fiona May',
+    school_year: 4,
+  },
+  {
+    name: 'Lufy',
+    favorite_class: 'One Piece',
+    school_year: 1,
+    subscribed_to_gorgeous_cyber: false,
+  },
+  {
+    name: 'Nami',
+    favorite_class: 'Treasure',
+    school_year: 10,
+    subscribed_to_gorgeous_cyber: false,
+  },
+  {
+    name: 'Zorrow',
+    favorite_class: 'Bounty Hunter',
+    school_year: 10,
+    subscribed_to_gorgeous_cyber: false,
+  },
+  {
+    name: 'Queen',
+    school_year: 4,
+  },
+];
--- index.js
+++ index.js
@@ -1,9 +1,81 @@
 const db = require('./configs/db_config');
+const Sequelize = require('sequelize');
+
+const demoStudents = require('./demo_students');
+
+const { Op } = Sequelize;
 
 (async () => {
   try {
-    await db.sequelize.sync({ force: true });
+    await db.sequelize.sync();
+    // *** Create or Delete Records ***
+    // await clearStudentTable();
+    // await bulkCreateStudents();
+    //////////////////////////////////////////
+
+    // *** Query ***
+    // Retrieve the name of every student whose their favorite class is Computer Science or they are subscribed to Gorgeous Cyber.
+    // const studentNames = await getComputerScienceStudentsOrSubscribersNames();
+    // studentNames.forEach((name) => console.log(name.toJSON()));
+
+    // Count the total amount of students in each school year and give the returned column the alias num_students.
+    const numberOfStudentsInEachSchoolYear =
+      await getNumberOfStudentsInEachSchoolYear();
+    numberOfStudentsInEachSchoolYear.forEach((numStudents) =>
+      console.log(numStudents.toJSON())
+    );
+
+    ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
   } catch (e) {
     console.log(e);
   }
 })();
+
+async function clearStudentTable() {
+  try {
+    await db.student.destroy({ truncate: true });
+  } catch (e) {
+    throw e;
+  }
+}
+
+async function bulkCreateStudents() {
+  try {
+    await db.student.bulkCreate(demoStudents, { validate: true });
+  } catch (e) {
+    throw e;
+  }
+}
+
+async function getComputerScienceStudentsOrSubscribersNames() {
+  try {
+    return await db.student.findAll({
+      attributes: ['name'],
+      where: {
+        [Op.or]: {
+          favorite_class: 'Computer Science',
+          subscribed_to_gorgeous_cyber: true,
+        },
+      },
+    });
+  } catch (e) {
+    throw e;
+  }
+}
+
+async function getNumberOfStudentsInEachSchoolYear() {
+  try {
+    return await db.student.findAll({
+      attributes: [
+        'school_year',
+        [
+          db.sequelize.fn('COUNT', db.sequelize.col('school_year')),
+          'num_students',
+        ],
+      ],
+      group: 'school_year',
+    });
+  } catch (e) {
+    throw e;
+  }
+}
